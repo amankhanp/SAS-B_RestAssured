ccc9b2f655e60efe1aa3da4cf6ca8228778a8fcb
amankhanp <pamanullahkhan.aman@gmail.com>
2019-12-23 14:35:38 +0530

    Added testng dependency and demo class with and empty method(will be update with practised code). //RestAssured

---
+++ src/test/java/com/codeaman/restassured/Test1_BasicFeatures.java
@@ -0,0 +1,16 @@
+package com.codeaman.restassured;
+
+import org.testng.annotations.Test;
+
+public class Test1_BasicFeatures {
+    /**
+     * In this class we are simply checking status code using Rest Assured.
+     */
+
+    @Test
+    public void testStatusCode(){
+        given()
+
+    }
+
+}
