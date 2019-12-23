107fcd74bb0925eeb67db48ac5e8202b5b5413ca
amankhanp <pamanullahkhan.aman@gmail.com>
2019-12-23 18:53:28 +0530

    Added some basic feature test for JSON & XML also added dependency of hamcrest.Matcher //RestAssured

---
+++ src/test/java/com/codeaman/restassured/Test1_BasicFeaturesForJSON.java
@@ -0,0 +1,80 @@
+package com.codeaman.restassured;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * In this class we are simply checking status code using Rest Assured for JSON.
+ */
+public class Test1_BasicFeaturesForJSON {
+
+    /**
+     * Simply we are hitting given api ("https://jsonplaceholder.typicode.com/posts/3").
+     * and then validating its response. That's all with this method.
+     */
+    @Test
+    public void testStatusCode(){
+        given().get("https://jsonplaceholder.typicode.com/posts/3").
+        then().statusCode(200);
+    }
+
+    /**
+     * Simply we are hitting given api ("https://jsonplaceholder.typicode.com/posts/3").
+     * and then validating its response and also Verifying code and and print complete response in console.
+     */
+    @Test
+    public void testLogging(){
+        given().get("https://jsonplaceholder.typicode.com/posts/3").
+                then().statusCode(200).log().all();
+    }
+
+    /**
+     * Simply we are hitting given api ("https://jsonplaceholder.typicode.com/posts/2")
+     * then verifying single content using org.hamcrest.Matchers library's equalTo method/function.
+     */
+    @Test
+    public void testEqualToFunction(){
+        given().get("https://jsonplaceholder.typicode.com/posts/2").
+                then().body("title", equalTo("qui est esse"));
+    }
+
+    /**
+     * Simply we are hitting given api ("https://jsonplaceholder.typicode.com/posts")
+     * then verifying multiple content using org.hamcrest.Matchers library's hasItems method/function.
+     */
+    @Test
+    public void testHasItemFunction(){
+        given().get("https://jsonplaceholder.typicode.com/posts").
+                then().body("id", hasItems(2,10,99));
+    }
+
+    /**
+     * Simply we are hitting given params, header.
+     * when calling api ("https://jsonplaceholder.typicode.com/posts/1")
+     * then validating its status code also printing its logs.
+     */
+    @Test
+    public void testParamsAndHeaders(){
+        given().
+                param("key1", "value1").header("headA","valueA").
+        when().
+                get("https://jsonplaceholder.typicode.com/posts/1").
+        then().
+                statusCode(200).log().all();
+    }
+
+    /**
+     * Simply we are hitting given params, header.
+     * when calling api ("https://jsonplaceholder.typicode.com/posts/1")
+     * And using 'and' to increase readability, generally used when writing in one line xpath style.
+     * without 'and' it doesn't make any difference its simply adds readability.
+     * And when calling api ("https://jsonplaceholder.typicode.com/posts/1")
+     * then validating its status code also printing its logs.
+     */
+    @Test
+    public void testAndFeatureForReadability    (){
+        given().param("key1", "value1").and().header("headA","valueA").when().get("https://jsonplaceholder.typicode.com/posts/1").then().statusCode(200).and().body("id", equalTo(1));
+    }
+
+}
---
+++ src/test/java/com/codeaman/restassured/Test1_BasicFeaturesForXML.java
@@ -0,0 +1,68 @@
+package com.codeaman.restassured;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * In this class we are simply checking status code using Rest Assured for XML.
+ */
+public class Test1_BasicFeaturesForXML {
+
+    /**
+     * Simply we are hitting given XML api ("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+     * and then validating single content response and printing all its response. That's all with this method.
+     */
+    @Test
+    public void testStatusCode(){
+        given().get("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+        then().body("CUSTOMER.CITY", equalTo("Dallas")).log().all();
+    }
+
+    /**
+     * Simply we are hitting given XML api ("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+     * and then validating multiple content response and printing all its response.
+     */
+    @Test
+    public void testMultipleContent(){
+        given().get("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+        then().
+            body("CUSTOMER.CITY", equalTo("Dallas")).
+            body("CUSTOMER.ID", equalTo("10")).
+            body("CUSTOMER.FIRSTNAME", equalTo("Sue")).
+            log().all();
+    }
+
+    /**
+     * Simply we are hitting given XML api ("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+     * then compare complete text in one go.
+     * This is not a good way to validate complete response in one go.
+     * it is just to understand we can do this too.
+     */
+    @Test
+    public void testCompleteTextInOneGo(){
+        given().get("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+        then().body("CUSTOMER.text()", equalTo("10SueFuller135 Upland Pl.Dallas"));
+    }
+
+    /**
+     * Simply we are hitting given XML api ("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+     * then xpaths can also be used to find the values and also used containsString method/function.
+     */
+    @Test
+    public void testUsingXpath1(){
+        given().get("http://thomas-bayer.com/sqlrest/CUSTOMER/10").
+        then().body(hasXPath("/CUSTOMER/FIRSTNAME", containsString("Sue")));
+    }
+
+    /**
+     * Simply we are hitting given XML api ("http://thomas-bayer.com/sqlrest/CUSTOMER").
+     * With xpath 'CUSTOMERList/CUSTOMER' we are chekcing some text which is '20' contains or not.
+     */
+    @Test
+    public void testUsingXpath2(){
+        given().get("http://thomas-bayer.com/sqlrest/CUSTOMER").
+        then().body(hasXPath("/CUSTOMERList/CUSTOMER[text()='20']")).log().all();
+    }
+}
