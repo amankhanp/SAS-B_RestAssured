2ac8bc49c331311d5e92dfe43294554cb7ae147b
amankhanp <pamanullahkhan.aman@gmail.com>
2020-01-07 15:33:55 +0530

    Updated CURD based class info.

--- src/test/java/com/codeaman/restassured/Test9_UsingCURD.java
+++ src/test/java/com/codeaman/restassured/Test9_UsingCURD.java
@@ -9,18 +9,30 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 
+/**
+ * In this class, we are using CURD, which stands for C-Create(Post), U-Update(Put), R-Read(get) and D-Delete(delete).
+ */
 public class Test9_UsingCURD {
 
+    /**
+     * In Before class method, we are giving baseURI, basePath and requestSpecification.
+     */
     @BeforeClass
     public void testSetUp(){
         RestAssured.baseURI = "https://petstore.swagger.io";
         RestAssured.basePath = "/v2";
-
         RestAssured.requestSpecification = new RequestSpecBuilder().
                 setContentType("application/json").build();
     }
 
-    @Test(priority = 0)
+    /**
+     * In this method, created 2 objects related to Pet.
+     * and in given body session we are passing pet object when we are posting a new request.
+     * post method will take the request from body and adds a new pet to the store as per API.
+     * in this serialization take place.
+     * using 'post' method - we 'create' a new request. CURD
+     */
+    @Test()
     public void testUsingPost() {
         Categories categoryDetails = new Categories(1, "Cat-Family");
         PetsDetails petsDetails = new PetsDetails(537, categoryDetails, "Jacky", "pending");
@@ -32,6 +44,10 @@ public class Test9_UsingCURD {
                 assertThat().statusCode(200);
     }
 
+    /**
+     * In this method, we are using put method to update the request.
+     * using 'put' method - we 'update' a request. CURD
+     */
     @Test(priority = 1)
     public void testUsingPut() {
         Categories categoryDetails = new Categories(1, "Cat-Family");
@@ -44,6 +60,12 @@ public class Test9_UsingCURD {
                 assertThat().statusCode(200);
     }
 
+    /**
+     * In this method, as we see in previous methods we have used given, when method to set
+     * baseURL, basePath or RequestSpecification but now in this class we have declared all the
+     * common details in beforeClass method,so we have started this method from static get method.
+     * using 'get' method - we 'read' a request. CURD
+     */
     @Test(priority = 2)
     public void testUsingGet() {
         get("/pet/537").
@@ -53,6 +75,10 @@ public class Test9_UsingCURD {
                 log().all();
     }
 
+    /**
+     * In this method we are just deleting the pet details which we have created using post method.
+     * using 'delete' method - we 'delete' a request. CURD
+     */
     @Test(priority = 3)
     public void testCaseDelete(){
         delete("/pet/537").
