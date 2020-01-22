e56c59493f1ccf012fcdee5586ede8fff8e1b9a7
amankhanp <pamanullahkhan.aman@gmail.com>
2020-01-22 13:23:17 +0530

    missing file for last commit 0a415d4

--- src/test/java/com/codeaman/restassured/Test12_Params.java
+++ src/test/java/com/codeaman/restassured/Test12_Params.java
@@ -1,9 +1,22 @@
 package com.codeaman.restassured;
 
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.hamcrest.Matcher;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
+/**
+ * Example1: http://api.fonts.com/rest/req?key1=val1!key2=val2, if we see
+ * at the end of the given url there is something like req?key1=val1 those
+ * are queries for validating them we use 'queryParam','formParam','param'.
+ *
+ * Example2: http://api.fonts.com/rest/{type}/{section}/, if we see at the
+ * end of the given url there is something like /{type}/{section}/ those are
+ * path's for validating them we use 'pathParam'.
+ */
 public class Test12_Params {
 
     /**
@@ -46,5 +59,42 @@ public class Test12_Params {
                 get("http://api.fonts.com/rest/json/Accounts/").
         then().
                 statusCode(400);
+    }
+
+    /**
+     * Path Param: for setting path we use 'pathParam'.
+     */
+    @Test
+    public void testPathParams(){
+        given().
+                pathParam("type", "json").
+                pathParam("section", "Domains").
+        when().
+                get("http://api.fonts.com/rest/{type}/{section}/").
+        then().
+                statusCode(400);
+    }
+
+    @Test
+    public void testBodyParamsUsing7(){
+        //validating body response using Java 7
+        given().
+                get("https://jsonplaceholder.typicode.com/photos/1").
+        then().
+                body("thumbnailUrl", new ResponseAwareMatcher<Response>() {
+                    @Override
+                    public Matcher<?> matcher(Response response) throws Exception {
+                        return equalTo("https://via.placeholder.com/150/92c952");
+                    }
+                });
+    }
+
+    @Test
+    public void testBodyParamsUsing8(){
+        //validating body response using Java 8 -> lambda expression
+        given().
+                get("https://jsonplaceholder.typicode.com/photos/1").
+        then().
+                body("thumbnailUrl", response -> equalTo("https://via.placeholder.com/150/92c952"));
     }
 }
